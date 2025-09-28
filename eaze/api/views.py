9ca8a1a4560ea5d74979e5095a06b0a42615db2b
nosheii <noshe1997@hotmail.com>
2025-09-28 14:55:02 +0200

    Start

---
+++ eaze/api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
+
+#http://localhost:8000/api/oppdaterNavn/
+@api_view(['POST'])
+def oppdaterNavn(request):
+    print("HEI JEG KJØRER I PYTHON")
+    return Response({"message": "Navn oppdatert!"})
